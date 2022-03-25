9154d68ce555c350e09f5e923a80c47258ed0fbc
BNewman917 <brentnewman917@gmail.com>
2022-03-25 16:50:20 -0500

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,5 @@ document.onmousemove = () => {
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
-    balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
   }
 };
